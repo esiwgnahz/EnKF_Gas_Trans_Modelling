cfc54764706722cb32ba7792d1537e4dfa96a309
eoe <642039627@qq.com>
2016-01-30 09:21:10 +0800

    main_function_modified_1.30

--- main_function.py
+++ main_function.py
@@ -6,7 +6,6 @@ __author__ = 'zq'
 import math
 from math import e
 import numpy as np
-import random as rd
 import pandas as pd
 import matplotlib.pyplot as plt
 import win32api
@@ -61,8 +60,9 @@ para_initial=para_init(sigma,deltax,deltay,dx,dy,m,n,Nod_num,N,ki_mean)
 np.savetxt('para_initial.txt',para_initial)
 ki_initial=np.exp(para_initial)
 
-
-def runexe(ki_initial,pi_initial,i):
+print time.ctime()
+#def runexe(ki_initial,pi_initial,i):
+for i in xrange(N):
     args_exe=r'E:\EnKF_Gas_Modelling\gas_{0}\ogs.exe'.format(i)
     args=r'E:\EnKF_Gas_Modelling\gas_{0}'.format(i)
     ki_modify=ki_initial[:,i]
@@ -70,23 +70,34 @@ def runexe(ki_initial,pi_initial,i):
     write_ki(Nod_num,ki_modify,i)  #para为初始参数，样本之间存在随机扰动
     write_p1(Nod_num,p1_initial[:,i],i)
     win32api.ShellExecute(0,'open',args_exe,'gas',args,0)
-    time.sleep(2)
-    Obs_p1=read_obs(Nod_num,obs_Num,i)
-    for ii in xrange(obs_num):
-        y_obs_prediction[ii][i]=Obs_p1[ii]  #组成观测点处的预测值矩阵
-    p1_predict=read_p(Nod_num,i)
-    for jj in xrange(Nod_num):
-        p_after[jj][i]=p1_predict[jj]     #组成状态变量预测值矩阵
-
-taskpool=pool.Pool(4)
-for i in xrange(N):
-    taskpool.spawn(runexe,ki_initial,p1_initial,i)
     print i
-taskpool.join()
+    time.sleep(2)
+    
+time.sleep(10)
+for k in xrange(N):
+    Obs_p1=read_obs(Nod_num,obs_Num,k)
+    for ii in xrange(obs_num):
+        y_obs_prediction[ii][k]=Obs_p1[ii]  #组成观测点处的预测值矩阵
+    p1_predict=read_p(Nod_num,k)
+    for jj in xrange(Nod_num):
+        p_after[jj][k]=p1_predict[jj]     #组成状态变量预测值矩阵
+    
 print time.ctime()
+#taskpool=pool.Pool(4)
+#for i in xrange(N):
+#    taskpool.spawn(runexe,ki_initial,p1_initial,i)
+#    print i
+#taskpool.join()
+#print time.ctime()
 
+
+
+
+
+# np.savetxt('y_obs_prediction.txt',y_obs_prediction)
+# np.savetxt('p_after.txt',p_after)
 x=np.vstack((para_initial,p_after))
-np.savetxt('x.txt',x)
+np.savetxt('x_1.txt',x)
 x_average=np.average(x,axis=1)
 # np.savetxt('x_average.txt',x_average)
 y_obs_pre_ava=np.average(y_obs_prediction,axis=1)
@@ -105,7 +116,7 @@ ph=np.dot(x_error,y_error.T)/(N-1)
 np.savetxt('ph.txt',ph)
 ph=np.matrix(ph)
 hph=np.dot(y_error,y_error.T)/(N-1)
-#np.savetxt('hph.txt',hph)
+# np.savetxt('hph.txt',hph)
 hph=np.matrix(hph+varR)
 hph_var=np.linalg.pinv(hph)
 #np.savetxt('hph_var.txt',hph_var)
@@ -123,7 +134,7 @@ np.savetxt('t_1.txt',xx)
 t_1_ave=xx.mean(axis=1)
 np.savetxt('t_1_ave.txt',t_1_ave)
 np.savetxt('yy.txt',yy)
-# np.savetxt('x_1.txt',x)
+np.savetxt('x_1_updated.txt',x)
 if np.any(xx>0):
     print 'there is positive parameter'
     exit()
@@ -148,23 +159,45 @@ except OverflowError:
 print time.ctime()
 
 
-for t in range(2,time_step+1):
+for t in range(2,21):
     try:
         KI=x[0:Nod_num,:]   #取出状态向量中的参数，将其进行指数恢复
         p1_update=x[Nod_num:Nod_num*2,:]
         KI=np.exp(KI)
-        # for i in xrange(N):    #采用restart方法的时候需要修改时间
-        #     time_modify(t,i)
+#        for i in xrange(N):
+#            time_modify(t,i)
 
-        taskpool1=pool.Pool(4)
-        for i in range(N):
-            taskpool1.spawn(runexe,KI,p1_update,i)
-            print i,'12'
-        taskpool1.join()
-
+#        taskpool1=pool.Pool(4)
+#        for i in range(N):
+#            taskpool1.spawn(runexe,KI,p1_initial,i)
+#            print i,'12'
+#        taskpool1.join()
+        for i in xrange(N):
+#            time_modify(t,i)
+            args_exe=r'E:\EnKF_Gas_Modelling\gas_{0}\ogs.exe'.format(i)
+            args=r'E:\EnKF_Gas_Modelling\gas_{0}'.format(i)
+            ki_modify=ki_initial[:,i]
+            ki_modify=para_keys_modify(ki_modify)
+            write_ki(Nod_num,ki_modify,i)  #para为初始参数，样本之间存在随机扰动
+            write_p1(Nod_num,p1_update[:,i],i)
+            win32api.ShellExecute(0,'open',args_exe,'gas',args,0)
+            print '{0}_{1}'.format(t,i)
+            time.sleep(5)
+        time.sleep(10)
+        
+        for k in xrange(N):
+            Obs_p1=read_obs(Nod_num,obs_Num,k)
+            for ii in xrange(obs_num):
+                y_obs_prediction[ii][k]=Obs_p1[ii]  #组成观测点处的预测值矩阵
+            p1_predict=read_p(Nod_num,k)
+            for jj in xrange(Nod_num):
+                p_after[jj][k]=p1_predict[jj]     #组成状态变量预测值矩阵
+            
+                
         np.savetxt('p_after_%d.txt'%t,p_after)
         np.savetxt('y_obs_prediction_%d.txt'%t,y_obs_prediction)
         x=np.vstack((para_initial,p_after))
+        np.savetxt('x_%d.txt' %t,x)
         x_average=np.average(x,axis=1)
         y_obs_pre_ava=np.average(y_obs_prediction,axis=1)
         x_error=np.zeros([Nod_num*2,N])
@@ -186,7 +219,7 @@ for t in range(2,time_step+1):
         hph_var=np.linalg.pinv(hph)
         k=np.dot(ph,hph_var)
         k=np.array(k)
-#        np.savetxt('k_%d.txt' %t,k)
+        np.savetxt('k_%d.txt' %t,k)
         yy=np.zeros([obs_num,N])
         for i in range(N):
             yy[:,i]=y_obs_p1[:,i]-y_obs_prediction[:,i]
@@ -199,11 +232,11 @@ for t in range(2,time_step+1):
         #     exit()
         # if np.any(p1_2<0):
         #     exit()
+        np.savetxt('x_%d_updated.txt' %t,x)
         np.savetxt('t_%d.txt' %t,xx)
         t_n_ave=xx.mean(axis=1)
         np.savetxt('t_%d_ave.txt' %t,t_n_ave)
-#        np.savetxt('x_%d.txt'%t,x)
-#        np.savetxt('yy_%d.txt' %t,yy)
+        np.savetxt('yy_%d.txt' %t,yy)
         parY[:,t-1]=np.average(x[0:Nod_num,:],axis=1)
         np.savetxt('parY.txt',parY)
         df=pd.DataFrame(parY[:,t-1])
